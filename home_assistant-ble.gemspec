7efb446b079fe59e2f5a6ba2dfc75640ef3497f4
Grégoire Seux <kamaradclimber@gmail.com>
2017-03-25 19:22:25 +0100

    First very simple version
    This version only print newly detected devices and those leaving

--- home_assistant-ble.gemspec
+++ home_assistant-ble.gemspec
@@ -23,4 +23,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 1.13'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rubocop'
+
+  spec.add_runtime_dependency 'ble'
 end
--- lib/home_assistant/ble.rb
+++ lib/home_assistant/ble.rb
@@ -1,7 +1,73 @@
 require 'home_assistant/ble/version'
+require 'ble'
+require 'mash'
 
 module HomeAssistant
   module Ble
-    # Your code goes here...
+    class Detector
+      attr_reader :config, :known_devices
+
+      def initialize(config)
+        @config = Mash.new(config)
+        @known_devices = {}
+      end
+
+      # polling interval
+      def interval
+        config['interval'] || 30
+      end
+
+      # time after which a device is considered as disappeared
+      def grace_period
+        config['grace_period'] || 60
+      end
+
+      def run
+        loop do
+          detect_devices
+          clean_devices
+          debug "Will sleep #{interval}s before relisting devices"
+          sleep interval
+        end
+      end
+
+      private
+
+      def log(message)
+        puts message
+      end
+
+      def debug(message)
+        puts message
+      end
+
+      def detect_devices
+        adapter.devices.each do |name|
+          log "Just discovered #{name}" unless known_devices.key?(name)
+          known_devices[name] = Time.now
+        end
+      end
+
+      def clean_devices
+        disappeared = (known_devices.keys - adapter.devices).select do |name|
+          Time.now - known_devices[name] > grace_period
+        end
+        disappeared.each do |name|
+          log "#{name} has disappeared"
+        end
+      end
+
+      def adapter
+        @adapter ||= begin
+                       iface = BLE::Adapter.list.first
+                       debug "Selecting #{iface} to listen for bluetooth events"
+                       raise 'Unable to find a bluetooth device'
+                       BLE::Adapter.new(iface).tap do |a|
+                         debug 'Activating discovery'
+                         a.start_discovery
+                       end
+                     end
+      end
+    end
   end
 end
